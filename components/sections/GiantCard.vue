888395943f5d72c65d5a718bf1047244b8a8d122
Wilfredo R Colina <wrca26@gmail.com>
2024-08-03 15:32:05 -0500

    Building GiantCardSection

---
+++ components/sections/GiantCard.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name: 'GiantCard',
+  props: {
+    title: {
+      type: String,
+      default: 'Prop Missing', // Corrected the default value
+    },
+    img: {
+      type: String,
+      default: 'Prop Missing', // Corrected the default value
+    },
+    description: {
+      type: String,
+      default: 'Prop Missing', // Corrected the default value
+    },
+    link: {
+      type: String,
+      default: '#', // Corrected the default value
+    },
+  },
+}
+</script>
+
+<template id="GiantCard">
+  <div class="grid grid-cols-1 md:grid-cols-5">
+    <figure class="col-span-3">
+      <img
+        class="max-w-[100%] max-h-[440px] max object-cover rounded-tl-[30px] rounded-br-[30px]"
+        :src="img"
+      />
+    </figure>
+    <div class="col-span-2 grid items-center md:ml-[-20%]">
+      <div
+        class="flex flex-col justify-center items-center bg-tertiary rounded-tl-[30px] rounded-br-[30px] py-20 px-5 gap-4"
+      >
+        <h2 class="text-3xl">{{ title }}</h2>
+        <p class="text-white mb-4">
+          {{ description }}
+        </p>
+        <a :href="link">
+          <button
+            class="text-primary bg-secondary hover:bg-secondary focus:ring-4 focus:outline-none font-medium rounded-lg text-lg px-4 py-2 text-center"
+          >
+            Learn more
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,13 @@
 import HomeHeader from '@/components/HomeHeader/HomeHeader.vue'
 import AustinOverview from '@/components/sections/AustinOverview.vue'
 import Highlights from '@/components/sections/Highlights/Highlights.vue'
+import GiantCard from '@/components/sections/GiantCard.vue'
 export default {
   components: {
     HomeHeader,
     AustinOverview,
     Highlights,
+    GiantCard,
   },
   data() {},
 }
@@ -15,9 +17,24 @@ export default {
 <template>
   <div>
     <HomeHeader />
-    <div class="max-w-6xl m-auto py-6 px-6">
+    <div class="max-w-6xl m-auto py-6 px-6 flex flex-col gap-24">
       <AustinOverview />
       <Highlights />
+      <GiantCard
+        title="Food & drink"
+        description="Life can be sweet. Or a little spicy. And sometimes, there’s french fries in your burrito. Whatever. It’s all good."
+        img="https://res.cloudinary.com/djf9jqpml/image/upload/v1722715864/Austin/krisztian-tabori-ZQf4jzkpz1k-unsplash_xpc7cb.jpg"
+      />
+      <GiantCard
+        title="Places to stay"
+        description="Austin offers places to stay that have just as much personality as the city itself. So no matter where you spend the night after a day in the Live Music Capital of the World®, you’re in for an authentic Austin experience."
+        img="https://res.cloudinary.com/djf9jqpml/image/upload/v1722716734/Austin/jeremy-doddridge-1jEWc28GxVs-unsplash_qcxaqz.jpg"
+      />
+      <GiantCard
+        title="Outdoor activities"
+        description="Austin is known for its great outdoors, both within the city limits and in the nearby rural Texas Hill Country. Get out and explore the best things to do outside, from winding hiking and biking trails to cool swimming holes and lakes perfect for paddling."
+        img="https://res.cloudinary.com/djf9jqpml/image/upload/v1722717033/Austin/tomek-baginski-2g7CAh4jzeY-unsplash_di1g4x.jpg"
+      />
     </div>
   </div>
 </template>
